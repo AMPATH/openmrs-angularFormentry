7c66e4511764b7712ca05803b8ad812fe04090e2
werick <jackericmugoma@gmail.com>
2015-11-18 12:40:23 +0300

    AA-345: Adding helper test methods

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -41,6 +41,7 @@ module.exports = function(config) {
       'bower_components/angular-ui-select/dist/select.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/bootstrap-ui-datetime-picker/dist/datetime-picker.min.js',
+      'bower_components/lodash/lodash.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.module.js',
