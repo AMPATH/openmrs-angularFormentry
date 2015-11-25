aa22c3b1022db9b93ed6379acc9b07928123fae7
jkariru <jkariru@ampath.or.ke>
2015-11-25 15:00:57 +0300

    Added  service test and  broadcast  to    inform  listeners of   schema  addition  attempts

--- app/scripts/formentry/services/fieldHandler.service.js
+++ app/scripts/formentry/services/fieldHandler.service.js
@@ -451,131 +451,132 @@
             }
 
             return service;
-
-            function _handleFieldUiSelect(_field, _answers) {
-                var field = _field || {};
-                var answerList = [];
-                answerList.push({name: '', value: undefined});
-                //get the anserq options for radio/select options/multicheckbox
-                _.each(_answers, function (answer) {
-                    var item = {
-                        name: answer.label,
-                        value: answer.concept
-                    };
-                    answerList.push(item);
-                });
-
-                field['templateOptions'] = {
-                    type: 'text',
-                    options: answerList
-                };
-            }
-
-            function _handleShowDate(_field) {
-                var field = _field || {};
-                var key = field.key;
-                field.key = key.replace(/obs/gi, 'obsDate');
-                field.type = 'datepicker';
-                field.templateOptions['datepickerPopup'] = 'dd-MMMM-yyyy';
-                field.templateOptions['label'] = 'Date';
-                field.expressionProperties = {
-                    'templateOptions.required': function ($viewValue, $modelValue, scope, element) {
-                        var value = $viewValue || $modelValue;
-                        var fkey = selField.key;
-                        return scope.model[fkey] !== undefined && scope.model[fkey] !== null && scope.model[fkey] !== '';
-                    }
-                };
-                field.validators = {
-                    dateValidator: '' //FormValidator.getDateValidatorObject(curField.validators[0]) //this  will require refactoring as we move forward
-                };
-            }
-
-            function _createFormlyFieldHelper(_question, model, _id) {
-                var field = {};
-                var m = {
-                    concept: _question.questionOptions.concept,
-                    schemaQuestion: _question, value: ''
-                };
-                var fieldKey = createFieldKey(_question, _id);
-                var _model = {};
-                _model[fieldKey] = m;
-                var key = _model[fieldKey]; //'value';
-                var keyNames = Object.keys(_model);
-                $log.debug('debug key ...', key);
-                field = {
-                    key: keyNames[0] + '.value',
-                    data: {concept: _question.questionOptions.concept,
-                        id: _question.id},
-                    type: 'input',
-                    templateOptions: {
-                        type: 'text',
-                        label: _question.label
-                    }
-                }
-                $log.debug('debug key field ...', field);
-                _handleExpressionProperties(field, _question.required, _question.disable);
-                _handleDefaultValue(field, _question.default);
-                _handleHide(field, _question.hide);
-                _handleValidators(field, _question.validators);
-
-                // if (_question.questionOptions.concept in model) { //add m to the array
-                if (fieldKey in model) { //add m to the array
-                    // model[_question.questionOptions.concept].push(m);
-                    model[fieldKey] = key;
-                } else { //create array with just m
-                    // model[_question.questionOptions.concept] = [m];
-                    model[fieldKey] = m;
-                }
-
-                // field.model = _model;
-                $log.debug('loosing value property', model);
-                return field;
-            }
-
-            function _addToQuestionMap(_question, _field, questionMap) {
-                if ('id' in _question) {
-                    if (_question.id in questionMap) {
-                        questionMap[_question.id].push(_field);
-                    } else {
-                        questionMap[_question.id] = [_field];
-                    }
-                }
-            }
-
-            function _createObsFormlyField(_question, _model, questionMap) {
-                var obsField = {};
-                obsField = _createFormlyFieldHelper(_question, _model, obsId);
-                if (_question.questionOptions.rendering === 'date') {
-                    obsField['type'] = 'datepicker';
-                    obsField['templateOptions']['datepickerPopup'] = 'dd-MMMM-yyyy';
-
-                } else if (_question.questionOptions.rendering === 'number') {
-                    obsField['templateOptions']['type'] = _question.questionOptions.rendering;
-                    obsField['templateOptions']['min'] = _question.questionOptions.min;
-                    obsField['templateOptions']['max'] = _question.questionOptions.max;
-
-                } else if ((_question.questionOptions.rendering === 'radio') ||
-                        (_question.questionOptions.rendering === 'select') ||
-                        (_question.questionOptions.rendering === 'multiCheckbox')) {
-                    _handleFieldAnswers(obsField, _question.questionOptions.answers);
-                    obsField['type'] = _question.questionOptions.rendering;
-                }
-
-                _addToQuestionMap(_question, obsField, questionMap);
-
-                var fieldArray = [];
-                var obsDateField;
-                if (_question.questionOptions.showDate === 'true') {
-                    obsDateField = angular.copy(obsField);
-                    _handleShowDate(obsDateField);
-                    fieldArray.push(obsField);
-                    fieldArray.push(obsDateField);
-                    return fieldArray;
-                } else {
-                    return obsField;
-                }
-            }
-
         }
-    }}
+
+        function _handleFieldUiSelect(_field, _answers) {
+            var field = _field || {};
+            var answerList = [];
+            answerList.push({name: '', value: undefined});
+            //get the anserq options for radio/select options/multicheckbox
+            _.each(_answers, function (answer) {
+                var item = {
+                    name: answer.label,
+                    value: answer.concept
+                };
+                answerList.push(item);
+            });
+
+            field['templateOptions'] = {
+                type: 'text',
+                options: answerList
+            };
+        }
+
+        function _handleShowDate(_field) {
+            var field = _field || {};
+            var key = field.key;
+            field.key = key.replace(/obs/gi, 'obsDate');
+            field.type = 'datepicker';
+            field.templateOptions['datepickerPopup'] = 'dd-MMMM-yyyy';
+            field.templateOptions['label'] = 'Date';
+            field.expressionProperties = {
+                'templateOptions.required': function ($viewValue, $modelValue, scope, element) {
+                    var value = $viewValue || $modelValue;
+                    var fkey = selField.key;
+                    return scope.model[fkey] !== undefined && scope.model[fkey] !== null && scope.model[fkey] !== '';
+                }
+            };
+            field.validators = {
+                dateValidator: '' //FormValidator.getDateValidatorObject(curField.validators[0]) //this  will require refactoring as we move forward
+            };
+        }
+
+        function _createFormlyFieldHelper(_question, model, _id) {
+            var field = {};
+            var m = {
+                concept: _question.questionOptions.concept,
+                schemaQuestion: _question, value: ''
+            };
+            var fieldKey = createFieldKey(_question, _id);
+            var _model = {};
+            _model[fieldKey] = m;
+            var key = _model[fieldKey]; //'value';
+            var keyNames = Object.keys(_model);
+            $log.debug('debug key ...', key);
+            field = {
+                key: keyNames[0] + '.value',
+                data: {concept: _question.questionOptions.concept,
+                    id: _question.id},
+                type: 'input',
+                templateOptions: {
+                    type: 'text',
+                    label: _question.label
+                }
+            }
+            $log.debug('debug key field ...', field);
+            _handleExpressionProperties(field, _question.required, _question.disable);
+            _handleDefaultValue(field, _question.default);
+            _handleHide(field, _question.hide);
+            _handleValidators(field, _question.validators);
+
+            // if (_question.questionOptions.concept in model) { //add m to the array
+            if (fieldKey in model) { //add m to the array
+                // model[_question.questionOptions.concept].push(m);
+                model[fieldKey] = key;
+            } else { //create array with just m
+                // model[_question.questionOptions.concept] = [m];
+                model[fieldKey] = m;
+            }
+
+            // field.model = _model;
+            $log.debug('loosing value property', model);
+            return field;
+        }
+
+        function _addToQuestionMap(_question, _field, questionMap) {
+            if ('id' in _question) {
+                if (_question.id in questionMap) {
+                    questionMap[_question.id].push(_field);
+                } else {
+                    questionMap[_question.id] = [_field];
+                }
+            }
+        }
+
+        function _createObsFormlyField(_question, _model, questionMap) {
+            var obsField = {};
+            obsField = _createFormlyFieldHelper(_question, _model, obsId);
+            if (_question.questionOptions.rendering === 'date') {
+                obsField['type'] = 'datepicker';
+                obsField['templateOptions']['datepickerPopup'] = 'dd-MMMM-yyyy';
+
+            } else if (_question.questionOptions.rendering === 'number') {
+                obsField['templateOptions']['type'] = _question.questionOptions.rendering;
+                obsField['templateOptions']['min'] = _question.questionOptions.min;
+                obsField['templateOptions']['max'] = _question.questionOptions.max;
+
+            } else if ((_question.questionOptions.rendering === 'radio') ||
+                    (_question.questionOptions.rendering === 'select') ||
+                    (_question.questionOptions.rendering === 'multiCheckbox')) {
+                _handleFieldAnswers(obsField, _question.questionOptions.answers);
+                obsField['type'] = _question.questionOptions.rendering;
+            }
+
+            _addToQuestionMap(_question, obsField, questionMap);
+
+            var fieldArray = [];
+            var obsDateField;
+            if (_question.questionOptions.showDate === 'true') {
+                obsDateField = angular.copy(obsField);
+                _handleShowDate(obsDateField);
+                fieldArray.push(obsField);
+                fieldArray.push(obsDateField);
+                return fieldArray;
+            } else {
+                return obsField;
+            }
+        }
+
+    }
+}
 )();
