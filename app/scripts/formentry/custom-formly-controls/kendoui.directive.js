4a79f0129a30f2fda8ba44c539862a3cfdea72ea
Nicky Kibor <nickykimaina@gmail.com>
2016-04-06 16:17:41 +0300

    ANFE-16: Added weeks control

--- app/scripts/formentry/custom-formly-controls/kendoui.directive.js
+++ app/scripts/formentry/custom-formly-controls/kendoui.directive.js
@@ -6,114 +6,130 @@ jscs:disable disallowMixedSpacesAndTabs, requireDotNotation, requirePaddingNewLi
 */
 (function() {
 
-  'use strict';
+    'use strict';
 
-  var mod =
-    angular
-    .module('openmrs.angularFormentry');
+    var mod =
+        angular
+            .module('openmrs.angularFormentry');
 
-  mod.run(function(formlyConfig) {
-    // Configure custom types
-    formlyConfig.setType({
-      name: 'kendo-date-picker',
-      // extends:"select",
-      wrapper: ['bootstrapLabel', 'bootstrapHasError', 'validation'],
-      template: '<div> ' +
-        '<input style="width: 100%" kendo-date-picker="myPicker" k-options="datePickerConfig" '+
-        'ng-model="$scope.selectModel" ng-click="open()"/> ' +
-        '</div> ',
+    mod.run(function(formlyConfig) {
+        // Configure custom types
+        formlyConfig.setType({
+            name: 'kendo-date-picker',
+            // extends:"select",
+            wrapper: ['bootstrapLabel', 'bootstrapHasError', 'validation'],
+            template:
+            '<div style="margin-right: 27px;">' +
+            '<div class="col-xs-11" style="margin: 0px; padding: 0px;">'+
+            '<input style="width:100%" kendo-date-picker="myPicker" k-options="datePickerConfig" ' +
+            'ng-model="$scope.selectModel" ng-click="open()"/> ' +
+            '</div>' +
+            '<div style="margin: 0px; padding: 0px;" ng-if="to.weeksList && to.weeksList.length > 0" class="col-xs-1">' +
+            '<button type="button" ng-disabled="to.disabled" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown" >'+
+            '<span class=""> Weeks </span> <span class="caret"></span></button>'+
+            '<ul class="dropdown-menu dropdown-menu-right">' +
+             '<li ng-repeat="week in to.weeksList"><a ng-click="setByWeeks(week)">{{week}} Weeks</a></li>'+
+            '</ul>'+
+            '</div>' +
+            '</div>' + 
+            '</br>',
 
-      controller: function($scope, $log, $timeout) {
-        var x = $scope.model[$scope.options.key.split('.')[0]];
-        
-        if(!_.isUndefined(x.value)) {
-          //format the date
-          x.value = kendo.toString(x.value, "yyyy-MM-dd HH:mm:ss+0300");
-        }
-        $scope.datePickerConfig = {
-          format : "dd-MM-yyyy",
-          parseFormats: ["yyyy-MM-ddTHH:mm:ss+0300", "yyyy-MM-dd HH:mm:ss+0300", "yyyy-MM-ddTHH:mm:ss.000Z", "yyyy-MM-ddTHH:mm:ss", "dd-MM-yyyy", "yyyy-MM-dd", "dd/MM/yyyy", "yyyy/MM/dd"],
-          change : function() {
-            var datePickerVal = this.value();
-            x.value = $scope.options.value(kendo.toString(datePickerVal, "yyyy-MM-dd HH:mm:ss+0300"));
-            console.log('test kendo', datePickerVal)
-            $scope.$digest();
-          }
-        };
+            controller: function($scope, $log, $timeout, moment) {
+                var x = $scope.model[$scope.options.key.split('.')[0]];
 
-        $scope.open = function() {
-            $scope.myPicker.open();
-        };
-      }
-    });
+                if (!_.isUndefined(x.value)) {
+                    //format the date
+                    x.value = kendo.toString(x.value, "yyyy-MM-dd HH:mm:ss+0300");
+                }
+                $scope.datePickerConfig = {
+                    format: "dd-MM-yyyy",
+                    parseFormats: ["yyyy-MM-ddTHH:mm:ss+0300", "yyyy-MM-dd HH:mm:ss+0300", "yyyy-MM-ddTHH:mm:ss.000Z", "yyyy-MM-ddTHH:mm:ss", "dd-MM-yyyy", "yyyy-MM-dd", "dd/MM/yyyy", "yyyy/MM/dd"],
+                    change: function() {
+                        var datePickerVal = this.value();
+                        x.value = $scope.options.value(kendo.toString(datePickerVal, "yyyy-MM-dd HH:mm:ss+0300"));
+                        console.log('test kendo', datePickerVal)
+                        $scope.$digest();
+                    }
+                };
 
-    formlyConfig.setType({
-      name: 'kendo-select-multiple',
-      // extends:"select",
-      wrapper: ['bootstrapLabel', 'bootstrapHasError', 'validation'],
-      template: '<div> ' +
-        '<select multiple="multiple"  kendo-multi-select k-options="selectOptions" ' +
-        'ng-model="$scope.model[$scope.options.key]" ></select> ' +
-        '</div> ',
+                $scope.open = function() {
+                    $scope.myPicker.open();
+                };
+                
+                $scope.setByWeeks = function(week) {
+                    var oneMonth = new moment().add(week, 'weeks');
+                     x.value = kendo.toString(oneMonth.toDate(), "yyyy-MM-dd HH:mm:ss+0300");
+                };
+            }
+        });
 
-      controller: function($scope, $log, $timeout) {
-        var x = $scope.model[$scope.options.key.split('.')[0]]
-          //can be used when using getterSetter provided by model options
-        $scope.selectModel = function(val) {
-          if (angular.isDefined(val)) {
-            x.value = val;
-          } else {
-            return x.value;
-          }
-        }; //$scope.model[$scope.options.key];
+        formlyConfig.setType({
+            name: 'kendo-select-multiple',
+            // extends:"select",
+            wrapper: ['bootstrapLabel', 'bootstrapHasError', 'validation'],
+            template: '<div> ' +
+            '<select multiple="multiple"  kendo-multi-select k-options="selectOptions" ' +
+            'ng-model="$scope.model[$scope.options.key]" ></select> ' +
+            '</div> ',
+
+            controller: function($scope, $log, $timeout) {
+                var x = $scope.model[$scope.options.key.split('.')[0]]
+                //can be used when using getterSetter provided by model options
+                $scope.selectModel = function(val) {
+                    if (angular.isDefined(val)) {
+                        x.value = val;
+                    } else {
+                        return x.value;
+                    }
+                }; //$scope.model[$scope.options.key];
 
 
-        $scope.selectOptions = {
-          dataTextField: 'name',
-          dataValueField: 'value',
-          valuePrimitive: true,
-          dataSource: $scope.to.options
-        };
-      }
-    });
+                $scope.selectOptions = {
+                    dataTextField: 'name',
+                    dataValueField: 'value',
+                    valuePrimitive: true,
+                    dataSource: $scope.to.options
+                };
+            }
+        });
 
-    formlyConfig.setType({
-      name: 'kendo-select',
-      // extends:"select",
-      wrapper: ['bootstrapLabel', 'bootstrapHasError', 'validation'],
-      template: '<div class="input-group"> ' +
-        '<select kendo-drop-down-list k-options="selectOptions"' +
-        'ng-model="$scope.model[$scope.options.key]" style="width: 100%;"></select>' +
-        '<div class="input-group-addon" ng-click="clearValue()">' +
-        '<span class="glyphicon glyphicon-remove"></span>' +
-        '</div>' +
-        '</div> ',
+        formlyConfig.setType({
+            name: 'kendo-select',
+            // extends:"select",
+            wrapper: ['bootstrapLabel', 'bootstrapHasError', 'validation'],
+            template: '<div class="input-group"> ' +
+            '<select kendo-drop-down-list k-options="selectOptions"' +
+            'ng-model="$scope.model[$scope.options.key]" style="width: 100%;"></select>' +
+            '<div class="input-group-addon" ng-click="clearValue()">' +
+            '<span class="glyphicon glyphicon-remove"></span>' +
+            '</div>' +
+            '</div> ',
 
-      controller: function($scope, $log, $timeout) {
-        var x = $scope.model[$scope.options.key.split('.')[0]]
-          //can be used when using getterSetter provided by model options
-        $scope.selectModel = function(val) {
-          if (angular.isDefined(val)) {
-            x.value = val;
-          } else {
-            return x.value;
-          }
-        }; //$scope.model[$scope.options.key];
+            controller: function($scope, $log, $timeout) {
+                var x = $scope.model[$scope.options.key.split('.')[0]]
+                //can be used when using getterSetter provided by model options
+                $scope.selectModel = function(val) {
+                    if (angular.isDefined(val)) {
+                        x.value = val;
+                    } else {
+                        return x.value;
+                    }
+                }; //$scope.model[$scope.options.key];
 
-        $scope.clearValue = function() {
-          x.value = null;
-        };
-        $scope.to.options.unshift({name:'',value:''});
-        $scope.selectOptions = {
-          dataTextField: 'name',
-          dataValueField: 'value',
-          valuePrimitive: true,
-          dataSource: $scope.to.options
-        };
-      }
-    });
+                $scope.clearValue = function() {
+                    x.value = null;
+                };
+                $scope.to.options.unshift({ name: '', value: '' });
+                $scope.selectOptions = {
+                    dataTextField: 'name',
+                    dataValueField: 'value',
+                    valuePrimitive: true,
+                    dataSource: $scope.to.options
+                };
+            }
+        });
 
-  })
+    })
 
 
 
