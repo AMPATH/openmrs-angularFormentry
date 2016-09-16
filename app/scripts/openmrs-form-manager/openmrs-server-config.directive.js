d37a0afd678102c17a5dd1894861bf1001e13e22
mhawila <a.mhawila@gmail.com>
2016-09-16 16:29:05 -0400

    ANFE-48: Added user session management

--- app/scripts/openmrs-form-manager/openmrs-server-config.directive.js
+++ app/scripts/openmrs-form-manager/openmrs-server-config.directive.js
@@ -27,12 +27,16 @@
     'SessionResService',
     '$log',
     'AuthService',
-    '$window'
+    '$window',
+    'dialogs',
+    '$timeout'
   ]
   
   function UrlConfigController($rootScope, $scope, $http, $base64, $cookies,
-    OpenmrsSettings, sessionService, $log, AuthService, $window) {
+    OpenmrsSettings, sessionService, $log, AuthService, $window, dialogs,
+    $timeout) {
     var restSuffix = 'ws/rest/v1/';
+    var SESSION_TTL_MIN = 10;
     
     $scope.openmrsUrl = $window.localStorage.getItem('openmrsUrl');
     if($scope.openmrsUrl === null || $scope.openmrsUrl == undefined || 
@@ -77,16 +81,14 @@
         OpenmrsSettings.setCurrentRestUrlBase(openmrsUrl + restSuffix);
         // Try to get session
         sessionService.getSession(function(data) {
-          $scope.authResult.authenticated = data.authenticated;
-          AuthService.authenticated(data.authenticated);
-          $cookies.put('sessionId',data.sessionId);
-          
-          // in case not authenticated
-          if(!data.authenticated) {
-            _setError(true, 'Invalid username and/or password')
-          } else {
-            // broadcast authentication event
+          if(data.authenticated) {
+            data.token = $base64.encode($scope.username + ':' + $scope.password);
+            $cookies.putObject('userSession', data, {
+              expires: _getSessionExpiryDate(SESSION_TTL_MIN),
+            });
             $rootScope.$broadcast('authenticated', data);
+          } else {
+            _setError(true, 'Invalid username and/or password');
           }
           $scope.busy = false;
         }, function(err) {
@@ -100,23 +102,104 @@
     
     $scope.logout = function() {
       sessionService.deleteSession(function(response) {
-        $http.defaults.headers.common.Authorization = null;
+        _clearCredentials();
         $scope.authResult.authenticated = false;
         $scope.authResult.hasError = false;
-        AuthService.authenticated(false);
+        AuthService.clearAuthentication();
+        $cookies.remove('userSession')
         $rootScope.$broadcast('deauthenticated');
       });
     }
     
     function _setCredentials(username, password) {
-      $http.defaults.headers.common.Authorization = 'Basic ' + 
-        $base64.encode(username + ':' + password);
+      if(arguments.length == 1) {
+        var base64Credos = username;
+      } else {
+        var base64Credos = $base64.encode(username + ':' + password);
+      }
+      $http.defaults.headers.common.Authorization = 'Basic ' + base64Credos;
+      base64Credos = null;
     }
     
+    function _clearCredentials() {
+      $http.defaults.headers.common.Authorization = null;
+    }
+
     function _setError(status, message) {
       $scope.authResult.hasError = status;
       $scope.authResult.message = message;
     }
     
+    function _checkIfSessionActive() {
+      var authData = $cookies.getObject('userSession');
+      if(authData) {
+        _setCredentials(authData.token);
+        $log.log('Credentials are set here');
+        $rootScope.$broadcast('authenticated', authData);
+      }
+    }
+    
+    function _getSessionExpiryDate(minutes) {
+      var d = new Date();
+
+      var minutesToSet = d.getMinutes() + minutes;
+      if(minutesToSet>59) {
+        d.setHours(d.getHours() + Math.floor(minutesToSet/60));
+        d.setMinutes(minutesToSet%60);
+      } else {
+        d.setMinutes(minutesToSet);
+      }
+      return d;
+    }
+
+    function _sessionTimeout(timeout, prevTimeoutPromise) {
+      var canceledSuccessfully = true;
+      if(prevTimeoutPromise) {
+        canceledSuccessfully = $timeout.cancel(prevTimeoutPromise);
+      }
+      if(!AuthService.authenticated()) return;
+      if(canceledSuccessfully) {
+        return $timeout(function(){
+          var TIME_WARNING_ACTIVE = 15; //seconds
+          var message = 'Your session will expire in ' + TIME_WARNING_ACTIVE
+                      + ' seconds, Stay in?'
+
+          var dlg = dialogs.confirm('Session Timeout', message);
+          var start = new Date();
+          dlg.result.then(function(btn) {
+            var end = new Date();
+            var elapsed = Math.floor((end.getTime() - start.getTime())/1000);
+            $log.debug('Time elapsed ' + elapsed);
+            if(elapsed > 15) {
+              $scope.logout();
+              dialogs.error('Session Expired!', 'Login to proceed');
+            }
+          }, function(btn) {
+            $scope.logout();
+          });
+        }, timeout, false);
+      }
+    }
+
+    $scope.$on('authenticated', function(event, data) {
+      $scope.authResult.authenticated = data.authenticated;
+      AuthService.authenticated(data);
+
+      var timeout = SESSION_TTL_MIN * 60 * 1000;    // to ms
+      var prevTimeoutPromise = _sessionTimeout(timeout);
+
+      document.addEventListener('mousemove', function(event) {
+        var obj = $cookies.getObject('userSession') || data;
+        $cookies.putObject('userSession', obj, {
+          expires: _getSessionExpiryDate(SESSION_TTL_MIN),
+        });
+
+        prevTimeoutPromise = _sessionTimeout(timeout, prevTimeoutPromise);
+      }, true);
+    });
+
+    if(!AuthService.authenticated()) {
+      _checkIfSessionActive();
+    }
   }
 })();
--- app/scripts/services/auth.service.js
+++ app/scripts/services/auth.service.js
@@ -21,23 +21,24 @@
       
       _this.authenticated = function(value) {
         if(angular.isDefined(value)) {
-          authData.authenticated = value;
+          if(typeof value != 'boolean') {
+            authData = value;
+          } else {
+            authData.authenticated = value;
+          }
         } else {
           return authData.authenticated;
         }
       }
       
-      $rootScope.$on('authenticated', function(event, data) {
-        if(data.user) {
-          if(data.user.name) {
-            authData.user.name = data.user.name;
-          }
-        }
-        authData.authenticated = true;
-      });
-      
-      $rootScope.$on('deauthenticated', function() {
-        authData.authenticated = false;
-      });
+      _this.clearAuthentication = function() {
+        authData = {
+          user: {
+            name: null,
+          },
+          authenticated: false,
+          sessionId: null,
+        };
+      }
     }
 })();
